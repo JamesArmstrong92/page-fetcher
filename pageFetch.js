38dc6c2e94f10356557631908e9d3bc1eaf5cea1
JamesArmstrong92 <jarmst23@gmail.com>
2021-03-10 19:13:16 -0500

    Refactored

--- pageFetch.js
+++ pageFetch.js
@@ -8,14 +8,13 @@ const path = arg[1];
 const pageFetch = function(url) {
 
   request(url, (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
     fs.writeFile(path, body, (err) => {
       if (err) {
         console.log(err);
       } else {
-        console.log('done!');
+        console.log(`Downloaded and saved to ${path}`);
       }
     });
   });
